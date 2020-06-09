006a8d6933b0718a785fe8bc560b9c77b96216fe
George Lester <glester491@gmail.com>
2020-06-08 22:52:53 -0700

    trying auto worker processes to avoid deadlock waiting for an old client to die

--- encode.sh
+++ encode.sh
@@ -6,5 +6,5 @@ name=$2
 /usr/bin/ffmpeg -re \
     -i rtmp://localhost/${app}/${name} \
     -async 1 -vsync -1 \
-    -c:v libx264 -c:a aac -b:v 1920k -b:a 128k -vf "scale=1280:trunc(ow/a/2)*2" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost/m3u8/${name}_720p \
-    &> /var/lib/streamrcv/data/hls/ffmpeg.txt
+    -c:v libx264 -c:a aac -b:v 1920k -b:a 128k -vf "scale=1280:trunc(ow/a/2)*2" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost/m3u8/${name}_720p
+    #&> /var/lib/streamrcv/data/hls/ffmpeg.txt
