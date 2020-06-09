fb9c9552b18c9de1e4467f064e5c93d09234e5ee
George Lester <glester491@gmail.com>
2020-06-08 21:41:28 -0700

    added ffmpeg log

--- encode.sh
+++ encode.sh
@@ -10,4 +10,4 @@ echo "encoding for ${app}/${name}" >> /var/lib/streamrcv/data/hls/wup.txt
     -async 1 -vsync -1 \
     -c:v libx264 -c:a libvo_aacenc -b:v 1024k -b:a 128k -vf "scale=960:trunc(ow/a/2)*2" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost/m3u8/${name}_sd \
     -c:v libx264 -c:a libvo_aacenc -b:v 1920k -b:a 128k -vf "scale=1280:trunc(ow/a/2)*2" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost/m3u8/${name}_720p \
-    -c copy -f flv rtmp://localhost/m3u8/${name}_src;
+    -c copy -f flv rtmp://localhost/m3u8/${name}_src &> /var/lib/streamrcv/data/hls/ffmpeg.txt
