942fc205c058995768d27cf7e538b0f54342184b
George Lester <glester491@gmail.com>
2020-06-08 22:43:16 -0700

    trying to remove src, seeing if it helps

--- encode.sh
+++ encode.sh
@@ -7,4 +7,4 @@ name=$2
     -i rtmp://localhost/${app}/${name} \
     -async 1 -vsync -1 \
     -c:v libx264 -c:a aac -b:v 1920k -b:a 128k -vf "scale=1280:trunc(ow/a/2)*2" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost/m3u8/${name}_720p \
-    -c copy -f flv rtmp://localhost/m3u8/${name}_src &> /var/lib/streamrcv/data/hls/ffmpeg.txt
+    &> /var/lib/streamrcv/data/hls/ffmpeg.txt
