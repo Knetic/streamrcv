5870d9d99218851a2d359ad1ec1a37363ff65c82
George Lester <glester491@gmail.com>
2020-06-09 14:30:27 -0700

    attempting to add auth

--- encode.sh
+++ encode.sh
@@ -6,4 +6,4 @@ name=$2
 /usr/bin/ffmpeg -re \
     -i rtmp://localhost/${app}/${name} \
     -async 1 -vsync -1 \
-    -c:v libx264 -c:a aac -b:v 1920k -b:a 128k -vf "scale=1280:trunc(ow/a/2)*2" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost/m3u8/${name}_720p
+    -c:v libx264 -c:a aac -b:v 1920k -b:a 128k -vf "scale=1280:trunc(ow/a/2)*2" -tune zerolatency -preset veryfast -crf 23 -f flv rtmp://localhost:1936/m3u8/${name}_720p
